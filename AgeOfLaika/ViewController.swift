2f55f151535ca2d2c246be47e04d89da92ea387d
Ewan <ewancluckie@gmail.com>
2015-02-21 17:25:52 +0700

    Added button to convert real dog years using control flow
    Added button to convert real dog years using control flow

--- AgeOfLaika/ViewController.swift
+++ AgeOfLaika/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var humanAgeTextField: UITextField!
+    @IBOutlet weak var dogYearsConvertedLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +23,42 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertToDogYearsButton(sender: UIButton) {
+        
+        let ageInTextField = humanAgeTextField.text.toInt()!
+        let conversionConstant = 7
+        dogYearsConvertedLabel.text = "You're furry companion is \(ageInTextField * conversionConstant) years old!"
+        humanAgeTextField.resignFirstResponder()
+        humanAgeTextField.text = ""
+    }
+    
+    @IBAction func convertToRealDogYearsButtonPressed(sender: UIButton) {
+        
+        // Get the text field entry and convert to Int
+        
+        let ageInTextField = humanAgeTextField.text.toInt()!
+        
+        // Set necessary variables and constants
+        
+        var ageAsDouble = Double(ageInTextField)
+        let youngDogConversionConstant = 10.5
+        let oldDogConversionConstant:Double = 4
+        
+        // Calculate real dog years based on age
+        
+        if ageAsDouble <= 2 {
+            dogYearsConvertedLabel.text = "You're furry companion is \(ageAsDouble * youngDogConversionConstant) years old!"
+        }
+        
+        else {
+            dogYearsConvertedLabel.text = "You're furry companion is \((2 * youngDogConversionConstant) + ((ageAsDouble - 2) * oldDogConversionConstant)) years old!"
+        }
+
+        // Housekeeping
+        
+        humanAgeTextField.text = ""
+        humanAgeTextField.resignFirstResponder()
+    }
 
 }
 
